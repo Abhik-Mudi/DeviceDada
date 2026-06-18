d0421b722034e978f5a59db8011cc7fef51de6ad
Abhik-Mudi <abhikmudi6@gmail.com>
2026-06-18 19:54:01 +0530

    production changes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  /* config options here */
   async rewrites() {
     return [
       {
-        // This matches any request starting with /api/ from your frontend
-        source: '/api/:path*', 
-        
-        // This forwards the request to your Express backend
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:4000'}/api/:path*`, 
+        source: '/api/:path*',
+        destination: `${process.env.BACKEND_URL || 'http://localhost:4000'}/api/:path*`,
       },
-    ]
+    ];
   },
 };
 
